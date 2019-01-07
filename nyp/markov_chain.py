9704323d58a0da698c1237d55ed2ee89c3ded36d
Drew McDonald <drewmcdo@gmail.com>
2019-01-07 18:21:39 -0500

    chain is building and producing probability dicts from a series

--- nyp/markov_chain.py
+++ nyp/markov_chain.py
@@ -2,23 +2,35 @@ from pandas import Series
 import numpy as np
 
 
+BEGIN = "___BEGIN__"
+END = "___END__"
+
+
+def counts_to_probability(counts: dict):
+    total = sum(counts.values())
+    probs = {}
+    for c in counts:
+        probs[c] = counts[c] / total
+    return probs
+
+
 class ChainData(object):
     """
     build a Markov Chain from a categorical Series
     
     - data: a pandas Series
-    - levels: MC depth (default 1)
+    - state_size: MC depth (default 1)
     - cull: impute an 'other' value in place of values with very little usage
 
     """
 
     def __init__(self, data: Series, state_size: int=1, cull: bool=True):
-        self.data = data.values
-        self.name = data.name if data.name else 'unnamed'
-        self.state_size = state_size
-        self.cull = cull
-        self.is_collapsed = False
-        self.prob: dict = None
+        self.data: Series = data.values
+        self.name: str = data.name if data.name else 'unnamed'
+        self.state_size: int = state_size
+        self.cull: bool = cull
+        self.is_collapsed: bool = False
+        self.prob: dict = self.collapse_chain()
 
     def __repr__(self):
         return f'<Chain ({self.name}): {{{self.sample_data_str}}}>'
@@ -30,20 +42,41 @@ class ChainData(object):
             return ", ".join(self.data[:4].tolist() + ["..."])
         return ", ".join(self.data)
 
-    def get_preprocessed(self):
-        """
-        pre-process the data (including culling) into a
-        clean character vector
-        """
+    def preprocess_data(self):
+        """pre-process the data (including culling) into a clean character vector"""
         return
 
-    def collapse_chain(self):
-        """collapse the categorical vector """
-        return
+    def collapse_chain(self) -> dict:
+        """collapse the categorical vector into a dict of probability lookups
+        loosely adapted from jsvine/markovify
+        """
+
+        lookup = {}
+
+        # accumulate counts of each input to output
+        for i in range(len(self.data)-self.state_size):
+
+            in_val = tuple(self.data[i:(i+self.state_size)])
+            out_val = self.data[i + self.state_size]
+
+            if in_val not in lookup:
+                lookup[in_val] = {}
+
+            if out_val not in lookup[in_val]:
+                lookup[in_val][out_val] = 0
+
+            lookup[in_val][out_val] += 1
+
+        # convert to probabilities
+        lookup = {k: counts_to_probability(v) for k, v in lookup.items()}
+
+        self.is_collapsed = True
+
+        return lookup
 
 
 if __name__=='__main__':
-    s = Series('a a b a b c'.split())
-    x = ChainData(s, levels=1)
+    s = Series('a a b a a b a b b c b a b c a b c b c a a c b c b a b b b b a c'.split())
+    x = ChainData(s, state_size=2)
 
-    print(x.collapse_chain())
+    print(x.prob)
