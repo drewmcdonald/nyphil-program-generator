f7a3a098c5519bd449181c96ef816abea262171c
Drew McDonald <drewmcdo@gmail.com>
2019-01-08 10:40:35 -0500

    adjusted chain a bit, and completed pre processing method

--- nyp/markov_chain.py
+++ nyp/markov_chain.py
@@ -1,36 +1,30 @@
 from pandas import Series
-import numpy as np
+from typing import Union
+
+OTHER = "___OTHER__"
 
 
-BEGIN = "___BEGIN__"
-END = "___END__"
-
-
-def counts_to_probability(counts: dict):
-    total = sum(counts.values())
-    probs = {}
-    for c in counts:
-        probs[c] = counts[c] / total
-    return probs
-
-
-class ChainData(object):
+class Chain(object):
     """
     build a Markov Chain from a categorical Series
     
     - data: a pandas Series
     - state_size: MC depth (default 1)
     - cull: impute an 'other' value in place of values with very little usage
-
+    - cull_threshold: the floor below which a value is replaced with OTHER; interpreted as a percentage of total records
+         if between 0 and .999; otherwise interpreted as a count of appearances if greater than or equal to 1
     """
 
-    def __init__(self, data: Series, state_size: int=1, cull: bool=True):
-        self.data: Series = data.values
-        self.name: str = data.name if data.name else 'unnamed'
+    def __init__(self, data: Series, state_size: int=1, cull: bool=True, cull_threshold: Union[int, float]=0.01):
+        self.raw_data: Series = data
+        self.name: str = data.name or 'unnamed'
         self.state_size: int = state_size
         self.cull: bool = cull
-        self.is_collapsed: bool = False
-        self.prob: dict = self.collapse_chain()
+        self.cull_threshold: Union[int, float] = cull_threshold
+
+        self.data: Series = self.pre_process_data(data)
+        self.counts: dict = self.fill_counts()
+        self.probas: dict = {k: self.counts_to_probabilities(v) for k, v in self.counts.items()}
 
     def __repr__(self):
         return f'<Chain ({self.name}): {{{self.sample_data_str}}}>'
@@ -42,13 +36,34 @@ class ChainData(object):
             return ", ".join(self.data[:4].tolist() + ["..."])
         return ", ".join(self.data)
 
-    def preprocess_data(self):
-        """pre-process the data (including culling) into a clean character vector"""
-        return
+    @classmethod
+    def counts_to_probabilities(cls, counts: dict):
+        total = sum(counts.values())
+        return {c: counts[c] / total for c in counts}
 
-    def collapse_chain(self) -> dict:
+    def pre_process_data(self, data: Series):
+        """pre-process the data (including culling) into a clean character vector"""
+        if self.cull:
+
+            if self.cull_threshold < 0:
+                raise ValueError('cull_threshold should be >= 0')
+
+            summary = data.value_counts()
+
+            # convert summary to percentages if needed
+            if 0 < self.cull_threshold < 1:
+                summary = summary / sum(summary)
+
+            keep = summary.index[summary >= self.cull_threshold]
+
+            data[~data.isin(keep)] = OTHER
+
+        return data
+
+    def fill_counts(self) -> dict:
         """collapse the categorical vector into a dict of probability lookups
         loosely adapted from jsvine/markovify
+        major difference is that this is set to accept a full corpus all at once with beginning/end markers imputed
         """
 
         lookup = {}
@@ -65,18 +80,16 @@ class ChainData(object):
             if out_val not in lookup[in_val]:
                 lookup[in_val][out_val] = 0
 
+            # increment
             lookup[in_val][out_val] += 1
-
-        # convert to probabilities
-        lookup = {k: counts_to_probability(v) for k, v in lookup.items()}
-
-        self.is_collapsed = True
 
         return lookup
 
 
 if __name__=='__main__':
-    s = Series('a a b a a b a b b c b a b c a b c b c a a c b c b a b b b b a c'.split())
-    x = ChainData(s, state_size=2)
+    s = Series('a a b a a b a b b c b a e f b c a e e b c b c a a c b c b a b b b b a c'.split())
+    x = Chain(s, state_size=3, cull_threshold=5)
 
-    print(x.prob)
+    print(x.probas)
+    print(x.counts)
+    print(x.data)
