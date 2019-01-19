80206f834109b3dba5cf80299b3469c14c1f0a44
drew <drewmcdo@gmail.com>
2019-01-19 13:27:51 -0500

    alter mbz composer load script for production run

--- scripts/2_mbz_composer_scrape.py
+++ scripts/2_mbz_composer_scrape.py
@@ -4,28 +4,25 @@ from sqlalchemy.orm import sessionmaker
 from nyp.raw.schema import Composer
 from nyp.mbz.composer import Base, MBZComposerSearch
 
-from shutil import copy2
-from os import remove
-from os.path import isfile
+# from shutil import copy2
+# from os import remove
+# from os.path import isfile
+
+# test_data = 'data/raw_test.db'
+# if isfile(test_data):
+#     remove(test_data)
+# copy2(production_data, test_data)
 
 production_data = 'data/raw.db'
-test_data = 'data/raw_test.db'
 
-this_data = test_data
-
-if isfile(test_data):
-    remove(test_data)
-
-copy2(production_data, test_data)
-
-engine = create_engine(f"sqlite:///{test_data}", echo=False)
+engine = create_engine(f"sqlite:///{production_data}", echo=False)
 Base.metadata.bind = engine
+Base.metadata.create_all()
 
 Session = sessionmaker(engine)
 s = Session()
-Base.metadata.create_all()
 
-for c in s.query(Composer).filter(Composer.id != 4).order_by(func.random()).limit(5):
+for c in s.query(Composer).filter(Composer.id != 4):
     print(c)
     search = MBZComposerSearch(c)
     search.pick_best_match()
@@ -33,3 +30,4 @@ for c in s.query(Composer).filter(Composer.id != 4).order_by(func.random()).limi
     if match:
         s.add(match)
         s.commit()
+
