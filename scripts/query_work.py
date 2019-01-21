6c788d4e4eed8fb3450e81a2ea32e77341d8acac
Drew McDonald <drewmcdo@gmail.com>
2019-01-20 19:06:52 -0500

    clean up query_work example

--- scripts/query_work.py
+++ scripts/query_work.py
@@ -1,8 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from collections import OrderedDict
 import re
 
-from nyp.models import ConcertSelection, EventType
 
 def matches_any(input_string: str, patterns: list) -> str:
     """detect if input_string matches any of patterns (return 'yes'), else return 'no'"""
@@ -12,8 +10,9 @@ def matches_any(input_string: str, patterns: list) -> str:
     return 'no'
 
 
-def matches_which(input_string: str, patterns: dict) -> [str, None]:
-    """given an input string and a dictionary of {'LABEL1': ['lab1pat1', 'lab1pat2'], 'LABEL2': ['lab2pat1', 'lab2pat2']},
+def matches_which(input_string: str, patterns: OrderedDict) -> [str, None]:
+    """given an input string and a dictionary of
+    {'LABEL1': ['lab1pat1', 'lab1pat2'], 'LABEL2': ['lab2pat1', 'lab2pat2']},
     return 'LABEL1' if either 'lab1pat1' or 'lab1pat2' matches to input string, etc."""
     for pattern_name in patterns:
         for sub_pattern in patterns[pattern_name]:
@@ -23,22 +22,24 @@ def matches_which(input_string: str, patterns: dict) -> [str, None]:
 
 
 if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from nyp.models import ConcertSelection, EventType
+
     Session = sessionmaker(create_engine("sqlite:///../data/raw.db"))
 
     s = Session()
-    q = s.query(ConcertSelection)\
-        .filter(EventType.name == 'Subscription Season')\
-        .slice(220, 230)
+    q = s.query(ConcertSelection) \
+        .filter(EventType.name == 'Subscription Season') \
+        .limit(10)
+
+    work_types = OrderedDict({'symphony': ['SYMPHONY', 'SINFONI'],
+                              'concerto': ['CONCERTO'],
+                              'overture': ['OVERTURE']})
 
     for r in q.all():
-        result = (r.selection.work.composer.name,
-                  r.selection.is_full_work,
-                  r.selection.work.title,
-                  [p.performer.instrument for p in r.performers if p.performer.instrument != 'Conductor'],
-                  # see https://en.wikipedia.org/wiki/Opus_number for other markers
+        result = (r.concert_id,
+                  r.selection_id,
                   matches_any(r.selection.work.title, ['BWV\. ?\d+', 'K\. ?\d+', 'OP\. ?\d+']),
-                  matches_which(r.selection.work.title, {'symphony': ['SYMPHONY', 'SINFONI'],
-                                                         'concerto': ['CONCERTO']}),
-                  'concerto' in r.selection.work.title.lower(),
-                  'symphony' in r.selection.work.title.lower())
+                  matches_which(r.selection.work.title, work_types))
         print(result)
