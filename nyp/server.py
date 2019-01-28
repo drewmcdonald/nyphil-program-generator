2e18f84f61f8014239a3afb3be64ddb0d6a01d8d
Drew McDonald <drewmcdo@gmail.com>
2019-01-27 20:50:29 -0500

    switched server to dict-style json return using Selection.to_dict()

--- nyp/server.py
+++ nyp/server.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import sessionmaker
 from nyp.markov import ChainEnsemble
 from nyp.models import Selection
 
+from typing import List
+
 import pickle
 
 model: ChainEnsemble = pickle.load(open('/Users/drew/Desktop/nyp/data/model_v1.p', 'rb'))
@@ -22,16 +24,28 @@ prediction_weights = {
     'percent_after_intermission_bin': 6.0
 }
 
+# this is pretty good!
+# ?percent_after_intermission_bin=100&work_type=50&soloist_type=25&composer_birth_century=25&composer_country=25
+
 
 @app.route('/generate', methods=['GET'])
 def generate_program():
     session = Session()
-    program = model.generate_program(prediction_weights, break_weight=60)
     q = session.query(Selection)
-    final_program = []
+
+    # parse request (just weights from command args for now)
+    weights = {k: int(v) for k, v in request.args.items()} or prediction_weights
+
+    program = model.generate_program(weights,
+                                     break_weight=60,
+                                     case_weight_exponent=.8)
+
+    final_program: List[Selection] = []  # receptacle
+
     for p in program:
-        final_program.append(str(q.get(int(p))))
-    session.close()
+        selection = q.get(int(p))
+        final_program.append(selection.to_dict())
+
     return jsonify(final_program)
 
 
